601e2f2f22a2136df3db9a60a1dd61bc3fa56c52
mileyphua <153359100+mileyphua@users.noreply.github.com>
2025-09-30 14:58:26 +0800

    Fix Vite base path for GitHub Pages deployment (#1)

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base: '/evevo-tech/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
