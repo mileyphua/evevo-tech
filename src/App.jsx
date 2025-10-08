d39bb9ff7ad363425307de91fa66fb35c4188b9f
mileyphua <153359100+mileyphua@users.noreply.github.com>
2025-10-08 06:51:10 -0400

    Fix: Update base path for custom domain (www.evevo-tech.com)

--- src/App.jsx
+++ src/App.jsx
@@ -605,7 +605,7 @@ function HomePage() {
 
 function App() {
   return (
-    <Router basename="/evevo-tech">
+    <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/industries" element={<Industries />} />
